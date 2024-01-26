2135743416145b02730a5da33417de69fd305f48
Rob1in <73953872+Rob1in@users.noreply.github.com>
2024-01-25 22:18:28 -0500

    add normalization

--- train.py
+++ train.py
@@ -25,6 +25,11 @@ def main(args):
     #Transforms
     test_transform = transforms.Compose([
         transforms.ToTensor(),
+        transforms.Normalize(
+       mean=[0.485, 0.456, 0.406],
+       std=[0.229, 0.224, 0.225]
+   ),
+        
     ])
 
     train_transform = transforms.Compose([
