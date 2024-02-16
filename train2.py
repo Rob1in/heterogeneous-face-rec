e0483562d05501f12059a298ff511af249f45d7e
Rob1in <73953872+Rob1in@users.noreply.github.com>
2024-02-16 16:59:51 -0500

    normalization around  0

--- train2.py
+++ train2.py
@@ -22,8 +22,9 @@ def main(cfg: DictConfig):
     test_transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize(
-       mean=[0.485, 0.456, 0.406],
-       std=[0.229, 0.224, 0.225]
+       #mean=[0.485, 0.456, 0.406],
+       mean=[0, 0, 0.],
+    #    std=[0.229, 0.224, 0.225]
    ),
         
     ])
